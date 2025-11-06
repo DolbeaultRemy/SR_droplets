fc36530102ca655d5ac78a960019c6e2fe9bf4c6
DolbeaultRemy <remy.dolbeault@ens-lyon.fr>
2025-11-06 17:56:03 +0100

    Finished with QuantumOptics for N=2 and N=8.

--- setup.jl
+++ setup.jl
@@ -1,10 +1,23 @@
+# import Pkg
+# Pkg.activate(".")
+# Pkg.add(Pkg.PackageSpec(name="QuantumCumulants", version="0.3.8"))
+# Pkg.add(Pkg.PackageSpec(name="OrdinaryDiffEq", version="6.98.0"))
+# Pkg.add(Pkg.PackageSpec(name="Symbolics", version="6.31.1"))
+# Pkg.add(Pkg.PackageSpec(name="QuantumOptics", version="1.2.3")) # Has to be 1.2.4 to use timeevolution.master! Conflict with CollectiveSpins.
+# Pkg.add("PhysicalConstants")
+# Pkg.add("Unitful")
+# Pkg.add("PyPlot")
+# Pkg.develop("CollectiveSpins")
+
 import Pkg
-Pkg.activate(".")
+Pkg.activate(".") # Change with working directory if you are not already inside
 Pkg.add(Pkg.PackageSpec(name="QuantumCumulants", version="0.3.8"))
+Pkg.add(Pkg.PackageSpec(name="JLD2", version="0.5.13"))
 Pkg.add(Pkg.PackageSpec(name="OrdinaryDiffEq", version="6.98.0"))
+Pkg.add(Pkg.PackageSpec(name="PyPlot", version="2.11.6"))
+Pkg.add(Pkg.PackageSpec(name="QuantumOptics", version="1.2.3"))
+Pkg.add(Pkg.PackageSpec(name="Statistics", version="1.11.1"))
 Pkg.add(Pkg.PackageSpec(name="Symbolics", version="6.31.1"))
-Pkg.add(Pkg.PackageSpec(name="QuantumOptics", version="1.2.3")) # Has to be 1.2.4 to use timeevolution.master! Conflict with CollectiveSpins.
-Pkg.add("PhysicalConstants")
-Pkg.add("Unitful")
-Pkg.add("PyPlot")
-#Pkg.develop("CollectiveSpins")
+
+# CollectiveSpins is outdated. Import it in dev mode, then go in you julia/dev directory and change the package by the one of this github (after unzipping it)
+Pkg.develop("CollectiveSpins")
